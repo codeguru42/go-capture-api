8f8bb8539036d5e33f1dbe53bd59f6b6e93fc8ba
Code-Apprentice <codeguru42@gmail.com>
2022-06-19 22:29:00 -0600

    Iterate over all files in inpuut directory

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+import os
 import sys
+from pathlib import Path
 
 import cv2
 
 
-def main(filename):
-    image = cv2.imread(filename)
+def contours(filename):
+    image = cv2.imread(str(filename))
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray, 30, 200)
     contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
-    cv2.imshow('Contours', image)
+    cv2.imshow(str(filename), image)
     cv2.waitKey()
+
+
+def main(directory):
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        contours(Path(directory) / filename)
     cv2.destroyAllWindows()
 
 
