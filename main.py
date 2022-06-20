dff1f04d50237561ff7e54088b05a9ee8daa37d4
Code-Apprentice <codeguru42@gmail.com>
2022-06-19 23:38:21 -0600

    Draw contours on black background

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import sys
 from pathlib import Path
 
 import cv2
+import numpy as np
 
 
 def contours(filename):
-    image = cv2.imread(str(filename))
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.imread(str(filename), cv2.IMREAD_GRAYSCALE)
+    blank_image = np.zeros((*gray.shape, 3))
     edges = cv2.Canny(gray, 30, 200)
     contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
-    cv2.imshow(str(filename), image)
+    cv2.drawContours(blank_image, contours, -1, (0, 255, 0), 3)
+    cv2.imshow(str(filename), blank_image)
     cv2.waitKey()
 
 
