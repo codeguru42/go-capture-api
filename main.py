623ea39087e48e399be947aa0dddc43cfa12e7ec
Code-Apprentice <codeguru42@gmail.com>
2022-06-19 22:20:20 -0600

    Draw contours on image

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import cv2
 
 def main(filename):
     image = cv2.imread(filename)
-    cv2.imshow('Image', image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 30, 200)
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+    cv2.imshow('Contours', image)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
