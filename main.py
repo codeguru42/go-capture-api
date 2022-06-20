4af6f55aa164ef43d0d75ede3811cac0155f704c
Code-Apprentice <codeguru42@gmail.com>
2022-06-20 00:04:07 -0600

    Write contour images to file instead of showing them in a window

--- main.py
+++ main.py
@@ -12,15 +12,13 @@ def contours(filename):
     edges = cv2.Canny(gray, 30, 200)
     contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     cv2.drawContours(blank_image, contours, -1, (0, 255, 0), 3)
-    cv2.imshow(str(filename), blank_image)
-    cv2.waitKey()
+    cv2.imwrite(str(Path('contours') / filename.name), blank_image)
 
 
 def main(directory):
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         contours(Path(directory) / filename)
-    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
