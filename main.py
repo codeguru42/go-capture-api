17fe0ad32cd75f66aa4088b3089ab8246f66dc1a
Code-Apprentice <codeguru42@gmail.com>
2022-06-19 22:11:36 -0600

    Open and display image with opencv

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import cv2
 
 
-# Press the green button in the gutter to run the script.
+def main(filename):
+    image = cv2.imread(filename)
+    cv2.imshow('Image', image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main(sys.argv[1])
