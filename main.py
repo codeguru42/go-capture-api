6ccc42e436ff32b3b4ffbf13c335d64a76c9f387
Code-Apprentice <codeguru42@gmail.com>
2022-06-26 20:39:24 -0600

    Separation of concerns
    find_contours() returns image
    main() writes image to disk

--- main.py
+++ main.py
@@ -6,19 +6,23 @@ import cv2
 import numpy as np
 
 
-def contours(filename):
+def find_contours(filename):
     gray = cv2.imread(str(filename), cv2.IMREAD_GRAYSCALE)
     blank_image = np.zeros((*gray.shape, 3))
     edges = cv2.Canny(gray, 30, 200)
     contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     cv2.drawContours(blank_image, contours, -1, (0, 255, 0), 3)
-    cv2.imwrite(str(Path('contours') / filename.name), blank_image)
+    return blank_image
 
 
 def main(directory):
+    if not os.path.exists('output'):
+        os.mkdir('output')
     for file in os.listdir(directory):
+        print(file)
         filename = os.fsdecode(file)
-        contours(Path(directory) / filename)
+        contours = find_contours(Path(directory) / filename)
+        cv2.imwrite(str(Path('output') / filename), contours)
 
 
 if __name__ == '__main__':
