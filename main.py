6b4caf3ebeafea2fee3465ecf04b6e8750584038
Code-Apprentice <codeguru42@gmail.com>
2022-07-04 20:08:17 -0600

    Find corners

--- main.py
+++ main.py
@@ -15,6 +15,18 @@ def find_contours(filename):
     return blank_image
 
 
+def find_corners(filename):
+    img = cv2.imread(str(filename))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+    # result is dilated for marking the corners, not important
+    dst = cv2.dilate(dst, None)
+    # Threshold for an optimal value, it may vary depending on the image.
+    img[dst > 0.01 * dst.max()] = [0, 255, 0]
+    return img
+
+
 def main(directory):
     output_root = Path('output')
     input_path = Path(directory)
@@ -22,10 +34,12 @@ def main(directory):
     for file in input_path.glob('*'):
         print(file)
         contours = find_contours(file)
+        corners = find_corners(file)
         output_path = output_root / file.stem
         output_path.mkdir(parents=True, exist_ok=True)
         shutil.copyfile(file, output_path / ('original' + file.suffix))
         cv2.imwrite(str(output_path / ('contours' + file.suffix)), contours)
+        cv2.imwrite(str(output_path / ('corners' + file.suffix)), corners)
 
 
 if __name__ == '__main__':
