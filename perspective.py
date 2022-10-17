0050a3393665b3462222207fc09a4e6921e0d691
Code-Apprentice <codeguru42@gmail.com>
2022-10-17 07:47:09 -0600

    Refactor with main() function

--- perspective.py
+++ perspective.py
@@ -45,23 +45,25 @@ def perspective_transform(image, corners):
     return cv2.warpPerspective(image, matrix, (width, height))
 
 
-image = cv2.imread(sys.argv[1])
-original = image.copy()
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blur = cv2.medianBlur(gray, 3)
-thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 3)
+def main(filename):
+    image = cv2.imread(filename)
+    original = image.copy()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.medianBlur(gray, 3)
+    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 3)
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.015 * peri, True)
+        transformed = perspective_transform(original, approx)
+        break
+    cv2.imshow('transformed', transformed)
+    cv2.imwrite('board.png', transformed)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
-cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
 
-for c in cnts:
-    peri = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.015 * peri, True)
-    transformed = perspective_transform(original, approx)
-    break
-
-cv2.imshow('transformed', transformed)
-cv2.imwrite('board.png', transformed)
-cv2.waitKey()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    main(sys.argv[1])
