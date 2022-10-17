d0490a874043c54ccdd6b84a56778283a49c3c76
Code-Apprentice <codeguru42@gmail.com>
2022-10-16 21:31:26 -0600

    Find lines

--- main.py
+++ main.py
@@ -27,19 +27,39 @@ def find_corners(filename):
     return img
 
 
-def main(directory):
-    output_root = Path('output')
-    input_path = Path(directory)
+def find_lines(filename):
+    gray = cv2.imread(str(filename), cv2.IMREAD_GRAYSCALE)
 
-    for file in input_path.glob('*'):
-        print(file)
-        contours = find_contours(file)
-        corners = find_corners(file)
-        output_path = output_root / file.stem
-        output_path.mkdir(parents=True, exist_ok=True)
-        shutil.copyfile(file, output_path / ('original' + file.suffix))
-        cv2.imwrite(str(output_path / ('contours' + file.suffix)), contours)
-        cv2.imwrite(str(output_path / ('corners' + file.suffix)), corners)
+    kernel_size = 5
+    blur = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
+
+    low_threshold = 50
+    high_threshold = 150
+    edges = cv2.Canny(blur, low_threshold, high_threshold)
+    rho = 1  # distance resolution in pixels of the Hough grid
+    theta = np.pi / 180  # angular resolution in radians of the Hough grid
+    threshold = 15  # minimum number of votes (intersections in Hough grid cell)
+    min_line_length = 50  # minimum number of pixels making up a line
+    max_line_gap = 20  # maximum gap in pixels between connectable line segments
+    lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
+
+    line_image = np.zeros((*gray.shape, 3))
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
+    return line_image
+
+
+def main(filename):
+    input_file = Path(filename)
+    output_root = Path('output')
+
+    print(input_file)
+    lines = find_lines(input_file)
+    output_path = output_root / input_file.stem
+    output_path.mkdir(parents=True, exist_ok=True)
+    shutil.copyfile(input_file, output_path / ('original' + input_file.suffix))
+    cv2.imwrite(str(output_path / ('lines' + input_file.suffix)), lines)
 
 
 if __name__ == '__main__':
