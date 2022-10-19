cf62ce38d6a212e36c35b81eda7f2340d77f8a46
Code-Apprentice <codeguru42@gmail.com>
2022-10-19 03:04:15 -0600

    Refactor find_stones()

--- make_sgf.py
+++ make_sgf.py
@@ -6,26 +6,38 @@ import numpy as np
 import perspective
 
 
+def find_stones(board):
+    width, height, _ = board.shape
+    dx = width // 18
+    dy = height // 18
+    radius = dx // 4
+    gray = cv2.cvtColor(board, cv2.COLOR_BGR2GRAY)
+    black = []
+    white = []
+    for x in range(19):
+        for y in range(19):
+            patch = gray[y * dy - radius:y * dy + radius, x * dx - radius:x * dx + radius]
+            average = np.average(patch)
+            if average < 50:
+                black.append((x, y))
+            elif average > 200:
+                white.append((x, y))
+    return black, white
+
+
 def main(filename):
     image = cv2.imread(filename)
     transformed = perspective.get_grid(image)
+    black, white = find_stones(transformed)
+
     width, height, _ = transformed.shape
     dx = width // 18
     dy = height // 18
     radius = dx // 4
-
-    gray = cv2.cvtColor(transformed, cv2.COLOR_BGR2GRAY)
-
-    for x in range(19):
-        for y in range(19):
-            average = np.average(gray[y*dy-radius:y*dy+radius,x*dx-radius:x*dx+radius])
-            if average < 50:
-                color = (255, 0, 0)
-            elif average > 200:
-                color = (0, 255, 0)
-            else:
-                color = (0, 0, 255)
-            cv2.circle(transformed, (x * dx, y * dy), radius, color, cv2.FILLED)
+    for x, y in black:
+        cv2.circle(transformed, (x * dx, y * dy), radius, (255, 0, 0), cv2.FILLED)
+    for x, y in white:
+        cv2.circle(transformed, (x * dx, y * dy), radius, (0, 255, 0), cv2.FILLED)
 
     cv2.imshow('transformed', transformed)
     cv2.waitKey()
