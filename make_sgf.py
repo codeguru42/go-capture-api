04272c27aecd87eeefefb860d65cf55719b9e0e6
Code-Apprentice <codeguru42@gmail.com>
2022-10-19 03:59:06 -0600

    Error handling

--- make_sgf.py
+++ make_sgf.py
@@ -49,23 +49,29 @@ def find_stones(board):
 
 
 def main(filename):
-    image = cv2.imread(filename)
-    transformed = perspective.get_grid(image)
-    black, white = find_stones(transformed)
+    try:
+        image = cv2.imread(filename)
+        transformed = perspective.get_grid(image)
 
-    width, height, _ = transformed.shape
-    dx = width // 18
-    dy = height // 18
-    radius = dx // 4
-    for x, y in black:
-        cv2.circle(transformed, (x * dx, y * dy), radius, (255, 0, 0), cv2.FILLED)
-    for x, y in white:
-        cv2.circle(transformed, (x * dx, y * dy), radius, (0, 255, 0), cv2.FILLED)
+        cv2.imshow('transformed', transformed)
+        cv2.waitKey()
 
-    cv2.imshow('transformed', transformed)
-    cv2.waitKey()
-    cv2.imwrite('board.png', transformed)
-    cv2.destroyAllWindows()
+        black, white = find_stones(transformed)
+
+        width, height, _ = transformed.shape
+        dx = width // 18
+        dy = height // 18
+        radius = dx // 4
+        for x, y in black:
+            cv2.circle(transformed, (x * dx, y * dy), radius, (255, 0, 0), cv2.FILLED)
+        for x, y in white:
+            cv2.circle(transformed, (x * dx, y * dy), radius, (0, 255, 0), cv2.FILLED)
+
+        cv2.imshow('transformed', transformed)
+        cv2.waitKey()
+        cv2.imwrite('board.png', transformed)
+    finally:
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
