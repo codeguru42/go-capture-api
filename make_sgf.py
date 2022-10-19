82247b60c80f6bd70374eba0c96d759c12b96f1b
Code-Apprentice <codeguru42@gmail.com>
2022-10-19 00:58:44 -0600

    Identify stones

--- make_sgf.py
+++ make_sgf.py
@@ -1,6 +1,7 @@
 import sys
 
 import cv2
+import numpy as np
 
 import perspective
 
@@ -12,12 +13,23 @@ def main(filename):
     dx = width // 18
     dy = height // 18
     radius = dx // 4
-    for x in range(width):
-        for y in range(height):
-            cv2.circle(transformed, (x * dx, y * dy), radius, (0, 255, 0), cv2.FILLED)
+
+    gray = cv2.cvtColor(transformed, cv2.COLOR_BGR2GRAY)
+
+    for x in range(19):
+        for y in range(19):
+            average = np.average(gray[y*dy-radius:y*dy+radius,x*dx-radius:x*dx+radius])
+            if average < 50:
+                color = (255, 0, 0)
+            elif average > 200:
+                color = (0, 255, 0)
+            else:
+                color = (0, 0, 255)
+            cv2.circle(transformed, (x * dx, y * dy), radius, color, cv2.FILLED)
+
     cv2.imshow('transformed', transformed)
-    cv2.imwrite('board.png', transformed)
     cv2.waitKey()
+    cv2.imwrite('board.png', transformed)
     cv2.destroyAllWindows()
 
 
