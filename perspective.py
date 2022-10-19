d42b4c3c34e2c3296698c772f42f8c308ecdd616
Code-Apprentice <codeguru42@gmail.com>
2022-10-18 23:35:53 -0600

    Factor out function

--- perspective.py
+++ perspective.py
@@ -38,19 +38,24 @@ def perspective_transform(image, corners):
     return cv2.warpPerspective(image, matrix, (width, height))
 
 
-def main(filename):
-    image = cv2.imread(filename)
-    original = image.copy()
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def get_corners(image):
+    copy = image.copy()
+    gray = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY)
     blur = cv2.medianBlur(gray, 3)
     thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 3)
     cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    return cnts
+
+
+def main(filename):
+    image = cv2.imread(filename)
+    cnts = get_corners(image)
     for c in cnts:
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.015 * peri, True)
-        transformed = perspective_transform(original, approx)
+        transformed = perspective_transform(image, approx)
         break
     cv2.imshow('transformed', transformed)
     cv2.imwrite('board.png', transformed)
