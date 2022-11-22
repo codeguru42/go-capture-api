7cea76a91413e7ac16ca205dbe3d30ea8e6e61cf
Code-Apprentice <codeguru42@gmail.com>
2022-11-22 01:04:57 -0700

    Save posted file

--- go_capture/views.py
+++ go_capture/views.py
@@ -1,6 +1,10 @@
+from django.core.files.storage import default_storage
 from django.http import HttpResponse
+from django.views.decorators.http import require_POST
 
 
+@require_POST
 def capture(request):
-    html = "<html><body>Hello, World!</body></html>"
-    return HttpResponse(html)
+    image_file = request.FILES['image']
+    default_storage.save(image_file.name, image_file)
+    return HttpResponse(status=201)
