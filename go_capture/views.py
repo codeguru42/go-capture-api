458e3bafec5d15be5255ebe6fb37d7d87d7f5680
Code-Apprentice <codeguru42@gmail.com>
2022-11-22 22:51:06 -0700

    Fix spelling

--- go_capture/views.py
+++ go_capture/views.py
@@ -5,4 +5,4 @@ from django.views.decorators.http import require_POST
 @require_POST
 def capture(request):
     image_file = request.FILES['image']
-    return FileResponse(image_file, filename="baord.jpg", status=201, as_attachment=True)
+    return FileResponse(image_file, filename="board.jpg", status=201, as_attachment=True)
