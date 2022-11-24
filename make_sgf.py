653271b92d9ce0a600b17009d2c1e30d7f85e91a
Code-Apprentice <codeguru42@gmail.com>
2022-11-23 20:45:18 -0700

    Factor out duplicate loop

--- make_sgf.py
+++ make_sgf.py
@@ -7,17 +7,19 @@ import find_stones
 import perspective
 
 
+def write_stones(file, stones):
+    for x, y in stones:
+        file.write(f'[{chr(x + ord("a"))}{chr(y + ord("a"))}]')
+
+
 def make_sgf(filename, black, white):
     with open(filename, 'w') as file:
         file.write('(;FF[4]\n')
         file.write('GM[1]\n')
         file.write(';AB')
-        for x, y in black:
-            file.write(f'[{chr(x+ord("a"))}{chr(y+ord("a"))}]')
-
+        write_stones(file, black)
         file.write('\n;AW')
-        for x, y in white:
-            file.write(f'[{chr(x+ord("a"))}{chr(y+ord("a"))}]')
+        write_stones(file, white)
         file.write('\n)')
 
 
