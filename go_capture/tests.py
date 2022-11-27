a21629db445940d5162f5f9bb05c8b03cea71c91
Code-Apprentice <codeguru42@gmail.com>
2022-11-27 11:56:51 -0700

    Parametrize filenames

--- go_capture/tests.py
+++ go_capture/tests.py
@@ -2,21 +2,26 @@ import io
 from pathlib import Path
 
 import cv2
+import pytest
 
 from go_capture.sgf import find_stones, make_sgf, perspective
 
 
-def test_whole_board():
-    input_path = Path('../images/whole_board')
-    for input_filename in input_path.glob('*'):
-        print(input_filename)
-        image = cv2.imread(str(input_filename))
-        board = perspective.get_grid(image)
-        black, white = find_stones.find_stones(board)
-        out_stream = io.StringIO()
-        make_sgf.make_sgf(out_stream, black, white)
-        expected_path = Path('../expected') / (input_filename.stem + '.sgf')
-        with expected_path.open() as expected_file:
-            expected = expected_file.readlines()
-        out_stream.seek(0)
-        assert ''.join(expected) == out_stream.read()
+def get_images(image_folder):
+    image_path = Path('../images/whole_board')
+    for image_filename in image_path.glob('*.jpg'):
+        yield image_filename
+
+
+@pytest.mark.parametrize('image_filename', list(get_images('../images/whole_board')), ids=str)
+def test_whole_board(image_filename):
+    image = cv2.imread(str(image_filename))
+    board = perspective.get_grid(image)
+    black, white = find_stones.find_stones(board)
+    out_stream = io.StringIO()
+    make_sgf.make_sgf(out_stream, black, white)
+    expected_path = Path('../expected') / (image_filename.stem + '.sgf')
+    with expected_path.open() as expected_file:
+        expected = expected_file.readlines()
+    out_stream.seek(0)
+    assert ''.join(expected) == out_stream.read()
