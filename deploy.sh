68b30d13a1077b75aad53e7fde2df2de596d9ac4
Code-Apprentice <codeguru42@gmail.com>
2023-02-02 06:45:20 +0000

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+image_hash=$1
+account_id=$(aws sts get-caller-identity --query Account --output text)
+aws ecr get-login-password | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-west-1.amazonaws.com
+docker run -d --env-file .env -p 8000:8000 --name go-capture ${account_id}.dkr.ecr.us-west-1.amazonaws.com/go-capture:${image_hash}
