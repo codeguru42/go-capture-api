b59ebb12f22311f2fa00f1b7695994ef58f12b1a
Code-Apprentice <codeguru42@gmail.com>
2023-02-02 06:57:06 +0000

    All steps must succeed

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 image_hash=$1
 account_id=$(aws sts get-caller-identity --query Account --output text)
-aws ecr get-login-password | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-west-1.amazonaws.com
-docker stop go-capture
-docker rm go-capture
-docker run -d --env-file .env -p 8000:8000 --name go-capture ${account_id}.dkr.ecr.us-west-1.amazonaws.com/go-capture:${image_hash}
-curl 127.0.0.1:8000/health_check/
+aws ecr get-login-password | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-west-1.amazonaws.com \
+  && docker stop go-capture \
+  && docker rm go-capture \
+  && docker run -d --env-file .env -p 8000:8000 --name go-capture ${account_id}.dkr.ecr.us-west-1.amazonaws.com/go-capture:${image_hash} \
+  && curl 127.0.0.1:8000/health_check/
