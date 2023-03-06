299bc3c9f8a01a70a04b325898b53e826a488fe8
Code-Apprentice <codeguru42@gmail.com>
2023-03-05 17:38:43 -0700

    Use docker compose for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-image_hash=$1
-account_id=$(aws sts get-caller-identity --query Account --output text)
+export image_hash=$1
+export account_id=$(aws sts get-caller-identity --query Account --output text)
 aws ecr get-login-password | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-west-1.amazonaws.com \
-  && docker stop go-capture \
-  && docker rm go-capture \
-  && docker run -d --env-file .env -p 8000:8000 --name go-capture ${account_id}.dkr.ecr.us-west-1.amazonaws.com/go-capture:${image_hash} \
+  && docker compose stop \
+  && docker compose build \
+  && docker compose run api
