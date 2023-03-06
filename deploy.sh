f10c41fa8c24dbe727b6dcf6944d00d80251d47c
Code-Apprentice <codeguru42@gmail.com>
2023-03-05 17:39:50 -0700

    Use down and up instead of stop and run to restart services

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 export image_hash=$1
 export account_id=$(aws sts get-caller-identity --query Account --output text)
 aws ecr get-login-password | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-west-1.amazonaws.com \
-  && docker compose stop \
+  && docker compose down \
   && docker compose build \
-  && docker compose run api
+  && docker compose up
