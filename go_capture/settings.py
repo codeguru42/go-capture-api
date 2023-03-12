fa81f21e4c465828cf662046271a215cf64aa2fb
Code-Apprentice <codeguru42@gmail.com>
2023-03-12 12:52:34 -0600

    Save image file to server

--- go_capture/settings.py
+++ go_capture/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+IMAGES_DIR = BASE_DIR / 'images'
+IMAGES_DIR.mkdir(exist_ok=True)
--- go_capture/views.py
+++ go_capture/views.py
@@ -1,7 +1,8 @@
 import io
 from pathlib import Path
 
-from django.http import FileResponse, JsonResponse
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST, require_GET
 
 from go_capture.sgf.process_image import process_image
@@ -10,17 +11,11 @@ from go_capture.sgf.process_image import process_image
 @require_POST
 def capture(request):
     image_file = request.FILES['image']
-    output_file = io.StringIO()
-    process_image(image_file, output_file)
-    output_file.seek(0)
-    filename = Path(image_file.name).stem
-    return FileResponse(
-        output_file.read(),
-        status=201,
-        filename=f'${filename}.sgf',
-        as_attachment=True,
-        content_type='application/x-go-sgf'
-    )
+    filename = Path(image_file.name)
+    output_path = settings.IMAGES_DIR / filename
+    with output_path.open('wb') as output_file:
+        output_file.write(image_file.read())
+    return HttpResponse(status=201)
 
 
 @require_GET
