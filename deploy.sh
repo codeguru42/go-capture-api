110b845e1498b7a10e9bb76f378028561ed5d3e7
Code-Apprentice <codeguru42@gmail.com>
2023-04-17 03:25:25 -0600

    Fix aws command

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export image_hash=$1
 export account_id=$(aws sts get-caller-identity --query Account --output text)
-aws echo "Login to ECR..." \
-  && ecr get-login-password | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-west-1.amazonaws.com \
+echo "Login to ECR..." \
+  && aws ecr get-login-password | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-west-1.amazonaws.com \
   && echo "Take down old api version..." \
   && docker compose down \
   && echo "Bring up new api version..." \
