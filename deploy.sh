057e4f2ad8704c94d8db86c0c8f089009f26bd18
Code-Apprentice <codeguru42@gmail.com>
2023-04-17 03:02:40 -0600

    Use docker-compose-prod.yml in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ export image_hash=$1
 export account_id=$(aws sts get-caller-identity --query Account --output text)
 aws ecr get-login-password | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-west-1.amazonaws.com \
   && docker compose down \
-  && docker compose up -d
+  && docker compose -f docker-compose-prod.yml -p go-capture-api -d up
