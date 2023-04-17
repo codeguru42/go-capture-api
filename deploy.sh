9e7de3e644b007993a5fc9abf3815371d1520128
Code-Apprentice <codeguru42@gmail.com>
2023-04-17 03:23:41 -0600

    Escape line breaks

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ export image_hash=$1
 export account_id=$(aws sts get-caller-identity --query Account --output text)
 aws echo "Login to ECR..." \
   && ecr get-login-password | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-west-1.amazonaws.com \
-  && echo "Take down old api version..."
+  && echo "Take down old api version..." \
   && docker compose down \
-  && echo "Bring up new api version..."
+  && echo "Bring up new api version..." \
   && docker compose -f docker-compose-prod.yml -p go-capture-api -d up
