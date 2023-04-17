e996711385bb6907edcfdbee02a59165d5fcb6cf
Code-Apprentice <codeguru42@gmail.com>
2023-04-17 03:22:24 -0600

    Add logging to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export image_hash=$1
 export account_id=$(aws sts get-caller-identity --query Account --output text)
-aws ecr get-login-password | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-west-1.amazonaws.com \
+aws echo "Login to ECR..." \
+  && ecr get-login-password | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-west-1.amazonaws.com \
+  && echo "Take down old api version..."
   && docker compose down \
+  && echo "Bring up new api version..."
   && docker compose -f docker-compose-prod.yml -p go-capture-api -d up
