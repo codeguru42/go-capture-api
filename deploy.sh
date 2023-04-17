5ff824d6f98f48364dd05612e56f8dd7612b0836
Code-Apprentice <codeguru42@gmail.com>
2023-04-17 03:27:04 -0600

    Specify file for `docker compose down` command

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ export account_id=$(aws sts get-caller-identity --query Account --output text)
 echo "Login to ECR..." \
   && aws ecr get-login-password | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-west-1.amazonaws.com \
   && echo "Take down old api version..." \
-  && docker compose down \
+  && docker compose -f docker-compose-prod.yml down \
   && echo "Bring up new api version..." \
   && docker compose -f docker-compose-prod.yml -p go-capture-api -d up
