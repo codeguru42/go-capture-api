02ef4bbcdfce359e20a75a63f1ee1c6cad3a33e1
Code-Apprentice <codeguru42@gmail.com>
2023-06-17 13:05:23 -0600

    No longer need to log into ECR since we are using Docker Hub

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 export image_hash=$1
-export account_id=$(aws sts get-caller-identity --query Account --output text)
-echo "Login to ECR..." \
-  && aws ecr get-login-password | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-west-1.amazonaws.com \
-  && echo "Take down old api version..." \
+echo echo "Take down old api version..." \
   && docker compose -f docker-compose-prod.yml down \
   && echo "Bring up new api version..." \
   && docker compose -f docker-compose-prod.yml -p go-capture-api up -d
