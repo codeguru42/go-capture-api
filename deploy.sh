e90ea8c8bfdb1c73db97ec2d991a81cf91053ebb
Code-Apprentice <codeguru42@gmail.com>
2023-06-17 13:06:51 -0600

    Add `tag` variable to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export image_hash=$1
+export tag=$2
 echo echo "Take down old api version..." \
   && docker compose -f docker-compose-prod.yml down \
   && echo "Bring up new api version..." \
