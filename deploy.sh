6af5e945d1a95e87b5629822b2abf9f311f30f83
Code-Apprentice <codeguru42@gmail.com>
2023-06-18 21:22:12 -0600

    Add env file to `docker compose down` command

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ export ENV_FILE=$1
 export IMAGE=$2
 export TAG=$3
 echo echo "Take down old api version..." \
-  && docker compose -f docker-compose-prod.yml down \
+  && docker compose -f docker-compose-prod.yml --env-file $ENV_FILE down \
   && echo "Bring up new api version..." \
   && docker compose -f docker-compose-prod.yml -p go-capture-api --env-file $ENV_FILE up -d
