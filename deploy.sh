33f5ec976ab8edf979f3d5dd64f62277ce5b4d25
Code-Apprentice <codeguru42@gmail.com>
2023-06-18 21:26:15 -0600

    Remove extra word from `echo` command

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 export ENV_FILE=$1
 export IMAGE=$2
 export TAG=$3
-echo echo "Take down old api version..." \
+echo "Take down old api version..." \
   && docker compose -f docker-compose-prod.yml --env-file $ENV_FILE down \
   && echo "Bring up new api version..." \
   && docker compose -f docker-compose-prod.yml -p go-capture-api --env-file $ENV_FILE up -d
