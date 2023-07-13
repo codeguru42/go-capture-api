ad1daf1654f5e0ff2093681a4ab13267f680b9b4
Code-Apprentice <codeguru42@gmail.com>
2023-07-13 12:20:03 -0600

    Implement /capture_async/ endpoint with fastapi

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import io
+from pathlib import Path
 from typing import Annotated
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 
+import settings
 from sgf.process_image import process_image
+from tasks import process_image_task
 
 app = FastAPI()
 
@@ -22,3 +25,15 @@ def capture(image: Annotated[UploadFile, File()]):
         'sgf': output_file.read(),
         'image_filename': image.filename,
     }
+
+
+@app.post('/capture_async/')
+def capture_async(
+        image: Annotated[UploadFile, File()],
+        fcm_registration_token: Annotated[str, Form()]
+):
+    filename = Path(image.filename)
+    output_path = settings.IMAGES_DIR / filename
+    with output_path.open("wb") as output_file:
+        output_file.write(image.file.read())
+    process_image_task.delay(str(output_path.absolute()), fcm_registration_token)
