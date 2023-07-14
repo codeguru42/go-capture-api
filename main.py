a3587becee642d5819699a5263aeca33072c0ab9
Code-Apprentice <codeguru42@gmail.com>
2023-07-13 21:07:54 -0600

    Fix code style with black

--- main.py
+++ main.py
@@ -11,26 +11,25 @@ from tasks import process_image_task
 app = FastAPI()
 
 
-@app.get('/health_check/')
+@app.get("/health_check/")
 def health_check():
     return {"message": "Healthy!"}
 
 
-@app.post('/capture/')
+@app.post("/capture/")
 def capture(image: Annotated[UploadFile, File()]):
     output_file = io.StringIO()
     process_image(image.file, output_file)
     output_file.seek(0)
     return {
-        'sgf': output_file.read(),
-        'image_filename': image.filename,
+        "sgf": output_file.read(),
+        "image_filename": image.filename,
     }
 
 
-@app.post('/capture_async/')
+@app.post("/capture_async/")
 def capture_async(
-        image: Annotated[UploadFile, File()],
-        fcm_registration_token: Annotated[str, Form()]
+    image: Annotated[UploadFile, File()], fcm_registration_token: Annotated[str, Form()]
 ):
     filename = Path(image.filename)
     output_path = settings.IMAGES_DIR / filename
